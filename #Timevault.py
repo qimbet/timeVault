10120343a1fde336c0babca0eef5a670a9072fac
Jacob <j_mattie@live.ca>
2024-06-17 06:54:20 -0600

    updated TimeVault for full SQL integration. All components are built; ready to begin debugging.

--- #Timevault.py
+++ #Timevault.py
@@ -1,19 +1,6 @@
 #Timevault
 #Jacob Mattie, May 13, 2024
 
-
-
-# ------------------------------------------------------------
-# Tasklist: 
-#
-# Update functions to use database features: encryptFile, DecryptFile, mostRecentIdentifier
-# For public release: require that encrypted files have the extension '.exe' (to lock out casual ransomware uses)
-# Develop a GUI to select files to be encrypted
-# 
-# ------------------------------------------------------------
-
-
-
 # This is code to encrypt a file for an indeterminate (user-set) amount of time; most typically in weeks scale. 
 # Security should be considered -- this is not a robust file encryption scheme, since all keys are visible within keyChain.
 # Keychain could be encrypted elsehow (consider private data center & API calls, should this be expanded for business use). 
@@ -22,13 +9,7 @@
 #Future improvements:
 	# Write a .bat script to parse timevault cells on computer startup (so files are decrypted when appropriate)
 	# On TimeVault startup, check jail times. If none are due for release, exit the program to save computer power.
-
-#SQL tables: 
-#1 table contains in each row:
-# BLOBFile, Identifier
-
-#2nd table contains, in each row: 
-#identifier, original file location, original filename, original file extension, releaseDate
+    # Develop a GUI to select files to be encrypted
 
 import os
 from cryptography.fernet import Fernet
@@ -45,7 +26,24 @@ global timeVaultInventory = timeVaultDirectory + inventoryString
 global jailTimeWeeks = 6 #default value; should be amenable to user-directed changes
 global maxNumberOfEncryptions = 1000
 
-targetFile = "LeagueofLegends.exe"
+fileName = "LeagueofLegends.exe"
+
+conn = sqlite3.connect("timeVault.db")
+cursor = conn.cursor()
+
+cursor.execute("""CREATE TABLE if NOT EXISTS jail(
+            identifier INT, 
+            convict BLOB, 
+            PRIMARY KEY(identifier))""")
+
+cursor.execute("""CREATE TABLE if NOT EXISTS wardenRecords(
+            identifier INT, 
+            originalFileLocation TEXT, 
+            originalFilename TEXT, 
+            releaseDate DATETIME, 
+            PRIMARY KEY(identifier))""")
+
+conn.commit()   #use this line whenever the table is updated
 
 # --------------------------------------------------------------------------------------------------------------------------
 
@@ -53,63 +51,78 @@ targetFile = "LeagueofLegends.exe"
 
 # --------------------------------------------------------------------------------------------------------------------------
 
-def encryptFile(key, identifier, originalFileDirectory, fileName, connection, cursor):  #copies and encrypts target file; 
+def encryptFile(identifier, key, originalFileDirectory, fileName, jailTimeWeeks, conn, cursor):  #copies and encrypts target file; 
     #record calculateRelease alongside information on: key, originalFileDirectory
     #timeVaultInventory: Encrypted files are stored here. This is the 'prison'
     encryptionMethod = Fernet(key)
-    saveStatus = False
 
     os.chdir(originalFileDirectory)
     encryptedData = encryptionMethod.encrypt(fileData)
 
     os.chdir(timeVaultInventory)
-    SQLOPERATION1 := add_to_table: encryptedData      #-------------------------------------------------- add to "BLOB-prisoner" colum
-    SQLOPERATION1 := add_to_table: identifier     #-------------------------------------------------
+
+    cursor.execute("""
+        INSERT INTO jail (identifier, convict)
+        VALUES (?, ?)
+    """(identifierInput, encryptedData))
+
+    cursor.execute("""
+        INSERT INTO wardenRecords (identifier, originalFileLocation, originalFileName, releaseDate)
+        VALUES (?, ?, ?, ?)
+    """(identifierInput, originalFileDirectory, fileName, calculateRelease(jailTimeWeeks)))    
+
     conn.commit()   #use this line whenever the table is updated
 
     os.chdir(originalFileDirectory)
-    os.remove(fileName)           #only removes the unencrypted files once the duplicate has been successfully saved
+    os.remove(fileName)           
     os.chdir(timeVaultDirectory)
 
-def decryptFile(key, identifier, connection, cursor):
+def decryptFile(identifierInput, key, conn, cursor):
     encryptionMethod = Fernet(key)
-    saveStatus = False
 
-    SQLOPERATION1 := search_by_identifier #-------------------------------------------------- identify row to decrypt
-    SQLOPERATION1 := fileData = lookup_BLOB-prisoner_file  #--------------------------------------------------
+    cursor.execute("""
+        SELECT convict
+        FROM jail
+        WHERE identifier = ?;    
+    """, (identifierInput,))
+    fileData = cursor.fetchone()
 
-    SQLOPERATION2 := lookup(fileName)
-    SQLOPERATION2 := lookup(fileExtension)
-    SQLOPERATION2 := lookup(originalDirectory)
-    conn.commit()   #use this line whenever the table is updated
+    cursor.execute("""
+        SELECT *
+        FROM wardenRecords
+        WHERE identifier = ?;    
+    """, (identifierInput,))
 
+    fileInfo = cursor.fetchall() #(identifier, originalDirectory, filename, releasedate)
+
+    originalDirectory = fileInfo[1]
+    fileName = fileInfo[2]
 
     decryptedFile = encryptionMethod.decrypt(fileData) 
     os.chdir(originalDirectory)
-    with open((fileName+fileExtension), "wb") as decryptedFile:
-        decryptedFile.write(decryptedData)
-        saveStatus = True
 
-    os.chdir(timeVaultInventory)
-    if (encryptSaveStatus == True):
-        os.remove(fileName + ".encrypted")           #only removes the encrypted file once the duplicate has been successfully restored
-        os.chdir(timeVaultDirectory)
-        SQLOPERATION1 remove rows
-        SQLOPERATION2 remove rows
+    with open((fileName), "wb") as decryptedFile:       #to be edited if we want to differentiate files based on type (e.g. .exe)
+        decryptedFile.write(decryptedData)
+
+        cursor.execute("DELETE FROM jail WHERE identifier = ?", (identifier,))
+        cursor.execute("DELETE FROM wardenRecords WHERE identifier = ?", (identifier,))
         conn.commit()   #use this line whenever the table is updated
 
+    os.chdir(timeVaultInventory)
+
 def checkRelease(conn, cursor):
-    
+    cursor.execute("""
+        SELECT key, identifier
+        FROM jail
+        WHERE releaseDate < DATE('now');    
+    """)
+    #date('now') function returns the date as text in this format: YYYY-MM-DD
+    results = cursor.fetchall()
+    return results
 
-def calculateRelease(timeIn, jailTimeWeeks): #returns date in YYYY-MM-DD. Can call for calculateRelease(timeIn, jailTimeWeeks).year
-    releaseDate = timeIn + datetime.timedelta(weeks=jailTimeWeeks)
+def calculateRelease(jailTimeWeeks): #returns date in YYYY-MM-DD. Can call for calculateRelease(timeIn, jailTimeWeeks).year
+    releaseDate = datetime.datetime.now() + datetime.timedelta(weeks=jailTimeWeeks)
     return releaseDate
-
-def checkSentenceDone(): #boolean return
-    if ((releaseDate) <= datetime.datetime.today()):
-        return False
-    else:
-        return True
 
 def generateKey():
     return Fernet.generate_key()
@@ -117,8 +130,14 @@ def generateKey():
 def newIdentifier(connection, cursor)
     while(true):
         testIdentifier = random.randint(1,maxNumberOfEncryptions) #this allows up to 1000 programs to be encrypted at a time
-        if(SQLOPERATION2 does not contain testIdentifier):
-            return identifier
+        cursor.execute("""
+                       SELECT 1
+                       FROM jail
+                       WHERE identifier = ?
+                    """, (testIdentifier,))
+        result = cursor.fetchone()
+        if result != 1: 
+            return testIdentifier
 
 # --------------------------------------------------------------------------------------------------------------------------
 
@@ -131,12 +150,22 @@ if not os.path.exists(timeVaultInventory): #create database tables during the in
     print("Folder 'Inventory' created!")
     os.chdir(timeVaultDirectory)
 
-startupMessage = f"Welcome to Timevault. \nCurrent timelock settings are for: {jailTimeWeeks} weeks\nCurrent target file directory is: {targetFileDirectory} \nYou are looking to encrypt: {targetFile}" + "\n\nPress 'enter' to begin.\n\nEnter 'edit' to change settings.\nEnter 'help' for more information."
+toRelease = checkRelease(conn, cursor) #checkRelease returns a list of tuples; (identifier, key)
+for element in toRelease:
+    decryptFile(element[0], element[1], conn, cursor)
+
+startupMessage = f"""Welcome to Timevault. \nCurrent timelock settings are for: {jailTimeWeeks} weeks\n
+                    Current target file directory is: {targetFileDirectory} \n
+                    You are looking to encrypt: {fileName} \n\n
+                    Press 'enter' to begin.\n\n
+                    Enter 'edit' to change settings.\n
+                    Enter 'help' for more information."""
+
 promptIterationMessage = "Continue.\nPress 'enter' to begin program, using values as defined previously"
 helpString = "help"
 editString = "edit"
 
-input(startupMessage) = userChoice
+userChoice = input(startupMessage)
 while True:
     if(upper.helpstring == upper.userChoice)
         print("Actually I haven't filled in the help section yet. Good luck.")
@@ -147,12 +176,6 @@ while True:
     if(len.userChoice == 0)
         break
 
-conn = sqlite3.connect("timeVault.db")
-cursor = conn.cursor()
-
-cursor.execute("CREATE TABLE if NOT EXISTS jail(identifier INT, jailFile BLOB, PRIMARY KEY(identifier))")
-cursor.execute("CREATE TABLE if NOT EXISTS wardenRecords(identifier INT, originalFileLocation TEXT, originalFilename TEXT, originalFileExtension TEXT, releaseDate DATETIME, PRIMARY KEY(identifier))")
-conn.commit()   #use this line whenever the table is updated
 
 key = generateKey()
 identifier = newIdentifier()
@@ -161,4 +184,4 @@ os.chdir(timeVaultInventory)
 
 encryptFile(key, identifier, targetFileDirectory, fileName, conn, cursor)
 
-decryptFile(key, identifier, conn, cursor)
+conn.close()
